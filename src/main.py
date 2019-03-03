149084f7f2b5ab1ca6c48e5712c31bd9ae014d28
TekRekon <45916994+TekRekon@users.noreply.github.com>
2019-03-03 12:24:48 -0500

    update .gitignore; implement bot startup and status

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.')
+
+TOKEN = open("secret.txt", "r")
+
+game = "CorruptReaktor"
 
 
+@client.event
+async def on_ready():
+    print("I am ready")
+    await client.change_presence(game=discord.Game(name="CorruptReaktor.py"), status=None, afk=False)
+
+
+client.run(TOKEN.read())
