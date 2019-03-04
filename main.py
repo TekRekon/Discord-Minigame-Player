21d21f5edc798c69346e20dd6fef8acce9e40a92
TekRekon <45916994+TekRekon@users.noreply.github.com>
2019-03-03 23:24:56 -0500

    testing

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    await client.add_reaction(message, emoji="⏫")
-    await client.add_reaction(message, emoji="⭐")
     await client.add_reaction(message, "⏬")
+    await client.add_reaction(message, emoji="⭐")
+    await client.add_reaction(message, emoji="⏫")
+
+@client.event
+async def on_member_update():
+    if discord.Member.display_name == "TekRekon":
+        await client.send_message(destination="514591632327442432", content="worked")
 
 client.run("NTEzODMyNzk3NjM5NTQwNzM5.D12Yzw.dUzzYz2y5k886Azzll2QPqMLeiM")
