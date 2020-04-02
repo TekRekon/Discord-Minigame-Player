da8d309f76f3c0d1a66aeaa573d4d0c92e2bc381
TekRekon <45916994+TekRekon@users.noreply.github.com>
2020-04-02 02:45:22 -0400

    Keep adding stuff to settings panel

--- Cogs/ConfigBot.py
+++ Cogs/ConfigBot.py
@@ -6,6 +6,7 @@ import Jsontools
 
 
 class ConfigBot(commands.Cog):
+    reactions = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯']
 
     def __init__(self, bot):
         self.bot = bot
@@ -32,7 +33,7 @@ class ConfigBot(commands.Cog):
         else:
             return False
 
-    def generateSettings(self, ctx):
+    def generateSettings(self, ctx, num):
         data = Jsontools.getDataParsable()
         settings_embed = discord.Embed(title=f'Hey {ctx.author.name}, what needs editing?', color=0xff0000)
         settings_embed.add_field(name='\u200b',
@@ -43,36 +44,90 @@ class ConfigBot(commands.Cog):
                                  value='🇧**:** Edit your mods: `' + str([x.name for x in userMods]) + '`',
                                  inline=False)
         settings_embed.set_author(name='Settings',
-                                  icon_url='https://cdn.discordapp.com/attachments/488700267060133889/694687205469323284/settingjif.gif')
+                                  icon_url='https://cdn.discordapp.com/attachments/488700267060133889'
+                                           '/694687205469323284/settingjif.gif')
         settings_embed.set_footer(text='React to continue')
         return settings_embed
 
+    async def addReactions(self, message, num):
+        for n, reaction in enumerate(ConfigBot.reactions):
+            if n < num:
+                await message.add_reaction(reaction)
+
+    async def sendSimpleEmbed(self, channel, text, delete):
+        sent_msg = await channel.send(
+            embed=discord.Embed(description=text, color=0xff0000))
+        if delete:
+            await sent_msg.delete(delay=10)
+        return sent_msg
+
     @commands.command()
     async def config(self, ctx):
-        await ctx.message.delete()
-        if ConfigBot.correct_command_use(ctx=ctx, admin_command=True):
-            sent_setting = await ctx.send(embed=ConfigBot.generateSettings(self, ctx))
-            await sent_setting.add_reaction('🇦')
+        try:
+            await ctx.message.delete()
+            if ConfigBot.correct_command_use(ctx=ctx, admin_command=True):
+                def check_reaction(reaction, user):
+                    return user == ctx.author and reaction.emoji in ConfigBot.reactions and reaction.message.id == sent_prompt.id
 
-            def check(reaction, user):
-                return user == ctx.author and reaction.emoji in ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮',
-                                                                 '🇯'] and reaction.message.id == sent_setting.id
+                def check_message(message):
+                    return message.author == ctx.author and len(
+                        message.content) > 0 and message.channel == ctx.message.channel
 
-            try:
-                reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60.0)
-                await sent_setting.delete()
-                if reaction.emoji == '🇦':
-                    await ctx.send("Worked!")
-            except asyncio.TimeoutError:
-                warn = await ctx.send(
-                    embed=discord.Embed(description=ctx.author.mention + ' Operation timed out', color=0xff0000))
-                await warn.delete(delay=20)
+                use_settings = True
+                while use_settings:
+                    embed = ConfigBot.generateSettings(self, ctx, 2)
+                    sent_prompt = await ctx.send(embed=embed)
+                    await ConfigBot.addReactions(self, sent_prompt, 2)
+                    reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check_reaction)
+                    await sent_prompt.delete()
 
-        else:
-            msg = await ctx.send(
-                embed=discord.Embed(title='\u200b', description=f'{ctx.author.mention} Access denied. Mod-Only.',
-                                    color=0xff0000))
-            await msg.delete(delay=10)
+                    if reaction.emoji == '🇦':
+                        sent_prompt = await ctx.send(
+                            embed=discord.Embed(description=f'{ctx.author.mention} Enter my new prefix',
+                                                color=0xff0000))
+                        command_change_response = await self.bot.wait_for('message', timeout=60.0, check=check_message)
+                        if len(command_change_response.content) < 6:
+                            Jsontools.changeData(ctx.guild.name, 'prefix', command_change_response.content)
+                        await sent_prompt.delete()
+                        await command_change_response.delete()
+
+                    if reaction.emoji == '🇧':
+                        sent_prompt = await ctx.send(embed=discord.Embed(
+                            description=f'{ctx.author.mention} React to continue: \n 🇦: Add a mod \n 🇧: Delete a mod',
+                            color=0xff0000))
+                        await ConfigBot.addReactions(self, sent_prompt, 2)
+                        reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check_reaction)
+                        await sent_prompt.delete()
+
+                        if reaction.emoji == '🇦':
+                            sent_prompt = await ctx.send(
+                                embed=discord.Embed(description=f'{ctx.author.mention} Mention the mod you want to add',
+                                                    color=0xff0000))
+                            mod_add_response = await self.bot.wait_for('message', timeout=60.0, check=check_message)
+                            await sent_prompt.delete()
+                            for member in mod_add_response.mentions:
+                                if member.id not in Jsontools.getData(ctx.guild.name, 'mods'):
+                                    Jsontools.addListVar(ctx.guild.name, 'mods', member.id)
+                            await mod_add_response.delete()
+                        if reaction.emoji == '🇧':
+                            sent_prompt = await ctx.send(embed=discord.Embed(
+                                description=f'{ctx.author.mention} Mention the mod you want to delete', color=0xff0000))
+                            mod_add_response = await self.bot.wait_for('message', timeout=60.0, check=check_message)
+                            await sent_prompt.delete()
+                            for member in mod_add_response.mentions:
+                                if member.id in Jsontools.getData(ctx.guild.name, 'mods'):
+                                    Jsontools.removeListVar(ctx.guild.name, 'mods', member.id)
+                            await mod_add_response.delete()
+
+
+            else:
+                await ConfigBot.sendSimpleEmbed(self, channel=ctx.message.channel,
+                                                text=f'{ctx.author.mention} Access denied. Mod-Only.', delete=True)
+
+        except asyncio.TimeoutError:
+            await ConfigBot.sendSimpleEmbed(self, channel=ctx.message.channel,
+                                            text=f'{ctx.author.mention} Operation timed out', delete=True)
+            ConfigBot.config.use_settings = False
 
     # @commands.command()
     # async def test(self, ctx, arg):
--- Jsontools.py
+++ Jsontools.py
@@ -1,27 +1,70 @@
 import json
 
 
-# Returns a variable representing our data file that we can search for keys/values
 def getDataParsable():
+    """Returns a variable representing our data file that we can search for keys/values"""
     with open('data.json', 'r') as f:
         data = json.load(f)
     return data
 
 
-# Given the location dict and key of a value, replace it with newVal
-def changeData(dict, key, new_val):
+def changeData(dict, key, value):
+    """Given the dictionary and key of a value, replace it with newVal"""
     with open('data.json', 'r') as f:
         data = json.load(f)
-    data[dict][key] = new_val
+    data[dict][key] = value
     with open('data.json', 'w') as f:
         json.dump(data, f, indent=4)
 
 
-# Add a dict for a new guild
+def getData(dict, key):
+    """Given a dictionary and key, return the value"""
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    return data[dict][key]
+
+
+def addKey(dict, key, value):
+    """Given a dictionary, create a new key with a new value"""
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    data[dict][key] = value
+    with open('data.json', 'w') as g:
+        json.dump(data, g, indent=4)
+
+
 def addGuild(guild):
+    """Add and initialize dictionary for a new guild"""
     with open('data.json', 'r') as f:
         data = json.load(f)
     data.update({guild.name: {'guildID': guild.id, "noCommandChannels": [], "mods": [guild.owner_id],
                               "prefix": "."}})
+    with open('data.json', 'w') as g:
+        json.dump(data, g, indent=4)
+
+
+def removeListVar(dict, list_key, var):
+    """Given a dictionary and key, remove an element from the corresponding list"""
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    data[dict][list_key].remove(var)
+    with open('data.json', 'w') as g:
+        json.dump(data, g, indent=4)
+
+
+def addListVar(dict, list_key, var):
+    """Given a dictionary and key, add an element to the corresponding list"""
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    data[dict][list_key].append(var)
+    with open('data.json', 'w') as g:
+        json.dump(data, g, indent=4)
+
+
+def renameKey(dict, old_key, new_key):
+    """Given a dictionary and a key, rename the key to new_key"""
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    data[dict][new_key] = data[dict].pop(old_key)
     with open('data.json', 'w') as g:
         json.dump(data, g, indent=4)
