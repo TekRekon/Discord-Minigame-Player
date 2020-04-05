46af430a674c823a64705dffa1182a5cc615ff47
Lukas Siernos <lukaSiernos@gmail.com>
2020-04-04 21:37:40 -0400

    Code cleanup

--- Cogs/CarouselStatus.py
+++ Cogs/CarouselStatus.py
@@ -4,21 +4,21 @@ import discord
 
 
 class CarouselStatus(commands.Cog):
-    statusNames = ['Made By TekRekon', 'CorruptReaktor.py', 'K̴̢̢̲̼̺̖̩̼̫̃̈́͛̑͌̄̚͠Í̵̢̬̺̠̼͖̲̫̲̦͘͝L̵̰̔L̶̛̘̟̖̪̺̜̈́̏̈́̄̾̎͘͝ ̵̡̎̏̑͌̈́́̈́̿̕͠͝͝E̸̛̝͕̹̝̰̲̣̗͊̑̏̂̀͜ͅͅṾ̴̖̪̥͍̫̓̇̈́͐͐͐̐̌͋͘͠ͅE̸͛̃͆̆̇̔̀̍̋̕͠͝ͅŖ̵̧̫̼̮͓̗̻̠͊̎̔̆̆̈̂̓ͅͅY̷̢̙̱̠̰̰̳͚͎̟̼̜͐̇̇̃̈͌͛̓͆͗Ö̴͎̲̥̺̦͎͓͎̳͉̹̪́̇̆̈́̋̀̊͛̒N̶̢̢̩̺̿̀͂̓͗̃̍̕͘E̸̢̟̟͕̫͚̯̟̞̮̫͓̔']
 
     def __init__(self, bot):
         self.bot = bot
         self.bot.loop.create_task(self.carousel_status())
 
     async def carousel_status(self):
+        statusNames = ['Made By TekRekon',
+                       'K̴̢̢̲̼̺̖̩̼̫̃̈́͛̑͌̄̚͠Í̵̢̬̺̠̼͖̲̫̲̦͘͝L̵̰̔L̶̛̘̟̖̪̺̜̈́̏̈́̄̾̎͘͝ ̵̡̎̏̑͌̈́́̈́̿̕͠͝͝E̸̛̝͕̹̝̰̲̣̗͊̑̏̂̀͜ͅͅṾ̴̖̪̥͍̫̓̇̈́͐͐͐̐̌͋͘͠ͅE̸͛̃͆̆̇̔̀̍̋̕͠͝ͅŖ̵̧̫̼̮͓̗̻̠͊̎̔̆̆̈̂̓ͅͅY̷̢̙̱̠̰̰̳͚͎̟̼̜͐̇̇̃̈͌͛̓͆͗Ö̴͎̲̥̺̦͎͓͎̳͉̹̪́̇̆̈́̋̀̊͛̒N̶̢̢̩̺̿̀͂̓͗̃̍̕͘E̸̢̟̟͕̫͚̯̟̞̮̫͓̔',
+                       f'{len(self.bot.guilds)} servers',
+                       f'Latency: {round(self.bot.latency*1000, 2)}ms']
         while True:
-            for n, x in enumerate(CarouselStatus.statusNames):
+            for x in statusNames:
                 await self.bot.change_presence(status=discord.Status.online,
                                                activity=discord.Game(x), afk=False)
-                if n % 2 == 0:
-                    await asyncio.sleep(2)
-                else:
-                    await asyncio.sleep(0.5)
+                await asyncio.sleep(5)
 
 
 def setup(bot):
